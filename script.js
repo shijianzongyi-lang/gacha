f059af1744a233a392f28cb6314ff49e3c80a2b9
Junaco <shijianzongyi@gmail.com>
2026-05-05 21:30:49 +0900

    script.js

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
 'use strict';
 
 const p = document.getElementById('output');
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+const third = document.getElementById('third');
+const start = document.getElementById('start');
+const stopp = document.getElementById('stop');
+const list = ["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ", "さ", "し", "す", "せ", "そ", "た", "ち", "つ", "て", "と", "な", "に", "ぬ", "ね", "の", "は", "ひ", "ふ", "へ", "ほ", "ま", "み", "む", "め", "も", "や", "ゆ", "よ", "わ", "を", "ん", "が", "ぎ", "ぐ", "げ", "ご", "ざ", "じ", "ず", "ぜ", "ぞ", "だ", "ぢ", "づ", "で", "ど", "ば", "び", "ぶ", "べ", "ぼ", "ぱ", "ぴ", "ぷ", "ぺ", "ぽ", "あ゛", "い゛", "う゛", "え゛", "お゛", "ぁ", "ぃ", "ぅ", "ぇ", "ぉ", "っ", "ゃ", "ゅ", "ょ"];
 
-document.querySelector('button').addEventListener('click', () => {
-  const text = document.getElementById('textInput');
-  const report = text.value;
+function setRandomA() {
+  let rand1 = Math.floor(Math.random()*(list.length-9));
+  first.textContent = list[rand1];
+}
+function setRandomB() {
+  let rand2 = Math.floor(Math.random()*list.length);
+  second.textContent = list[rand2];
+}
+function setRandomC() {
+  let rand3 = Math.floor(Math.random()*list.length);
+  third.textContent = list[rand3];
+}
 
-  //日本語の正規表現
-  const PJ1 = new RegExp(/受か|合格|うか|ごうかく/);
-  const PJ2 = new RegExp(/ません/);
-  const PJ3 = new RegExp(/なかっ/);
-  const NJ1 = new RegExp(/落ち|不合格|おち|ふごうかく|滑|すべ/);
-  //英語の正規表現
-  const PE1 = new RegExp(/pass|succeed/);
-  const NE1 = new RegExp(/disqualifi|rejected|fail|unsuccessful/)
-
-  //判定する
-  let a = 0
-  if (PJ1.test(report) == true) {
-    if (PJ2.test(report) == true) {
-      a = -1;
-    } else if (PJ3.test(report) == true) {
-      a = -1;
-    } else if (NJ1.test(report) == true) {
-      a = -1;
-    } else {
-      a = 1;
-    }
-  } else if (NJ1.test(report) == true) {
-    a = -1;
-  } else if (PE1.test(report) == true) {
-    a = 2;
-  } else if (NE1.test(report) == true) {
-    a = -2;
-  }
-
-  //反応のリスト
-  const a1 = ['合格おめでとう！！', '合格おめでとうございます。貴方様のますますのご発展をお祈り申し上げます。', 'すごい！めっちゃ頑張ったね！！', '合格、誠におめでとうございます。今後一層のご活躍とご健康を祈念いたします'];
-  const a2 = ['頑張りは必ず誰かが見ているよ', 'あなたのこれまでの努力は必ず結果につながる', 'どんな結果でも、あなたの価値は変わらない', 'あなたが挑戦した時点で、もうすばらしい']
-  const a3 = ['Cogratulations on passing your exams!!', 'Excellent!!', 'Wonderful!!', 'Fantastic!!'];
-  const a4 = ['You\'re gonna do fine!', 'It\'s OK, cheer up!', 'I\'m on your side.', 'We\'ll support you anyway we can.']
-
-  //判定後の処理
-  const arg = Math.floor(Math.random() * 4);
-  //console.log(arg);
-  if (a == 1) {
-    p.textContent = a1[arg];
-  } else if (a == -1) {
-    p.textContent = a2[arg];
-  } else if (a == 2) {
-    p.textContent = a3[arg];
-  } else if (a == -2) {
-    p.textContent = a4[arg];
-  } else {
-    setTimeout(() => {
-      location.href = 'https://abehiroshi.la.coocan.jp/';
-    }, 1000); //1秒待機
-    p.textContent = 'ページを移動します. . .';
-  }
+let timer1;
+let timer2;
+let timer3;
+start.addEventListener('click', () => {
+  start.disabled = true;
+  start.classList.add('disabledbtn');
+  stopp.classList.remove('disabledbtn');
+  first.classList.remove('under');
+  second.classList.remove('under');
+  third.classList.remove('under');
+  timer1 = window.setInterval(setRandomA, 50);
+  timer2 = window.setInterval(setRandomB, 50);
+  timer3 = window.setInterval(setRandomC, 50);
 });
+
+stopp.addEventListener('click', () => {
+  stopp.classList.add('disabledbtn');
+  window.clearInterval(timer1);
+  first.classList.add('under');
+  //setTimeout(() => {
+    //first.classList.remove('show');
+  //}, 500);
+  setTimeout(() => {
+    window.clearInterval(timer2);
+    second.classList.add('under');
+  }, 1000);
+  setTimeout(() => {
+    window.clearInterval(timer3);
+    third.classList.add('under');
+    //Xへの共有
+    const firstFigure = document.getElementById("first").textContent;
+    const secondFigure = document.getElementById("second").textContent;
+    const thirdFigure = document.getElementById("third").textContent;
+    const share_twitter = document.getElementById("js-share-twitter");
+    const share_title = `私の今日の３文字は「${firstFigure}${secondFigure}${thirdFigure}」`
+    share_twitter.setAttribute(
+	    "href",
+	    "https://twitter.com/share?url=" + "https://shijianzongyi-lang.github.io/wanwan/" + "&text=" + share_title + "&hashtags=今日の３文字"
+    );
+    start.classList.remove('disabledbtn');
+    start.disabled = false;
+  }, 2000);
+})
